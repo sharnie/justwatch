cbe69722d2b35199de68113d7c7bae29abdf3487
natachas <natacha.springer@gmail.com>
2014-07-14 14:18:03 -0400

    removed HEAD

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -50,10 +50,7 @@ class GistsController < ApplicationController
     render file: "gists/embed_stylesheet.css"
   end
 
-<<<<<<< HEAD
 
-=======
->>>>>>> natacha2
 private
 
   def set_gist_and_visual
