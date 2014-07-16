1b741ffb707e8767fcc47ba0c1757de379e131ff
natachas <natacha.springer@gmail.com>
2014-07-16 19:53:01 -0400

    guest_user created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+def current_user
+  super || guest_user
+end
+
+private
+
+def guest_user
+ User.find(session[:guest_user_id].nil? ? session[:guest_user_id] = create_guest_user.id : session[:guest_user_id])
+end
+
+def create_guest_user
+  u = User.create(:email => "guest_#{Time.now.to_i}#{rand(99)}@example.com")
+  u.save(:validate => false)
+  u
+end
+
 end
