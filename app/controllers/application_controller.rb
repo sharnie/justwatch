4fd30c0c22b21affddf9c25c479f2720204dcbe8
natachas <natacha.springer@gmail.com>
2014-07-17 15:44:28 -0400

    removed rogue symbol

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-<<<<<<< HEAD
-
   include GuestHelper
 
-
-
-=======
->>>>>>> destroy_button
 end
