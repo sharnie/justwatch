763448b1862f3b4b25d0bfb917a628051c2a8473
natachas <natacha.springer@gmail.com>
2014-07-23 18:57:40 -0400

    changed precompile directive for new page

--- app/assets/javascripts/gists/new/index.js
+++ app/assets/javascripts/gists/new/index.js
@@ -3,5 +3,5 @@
 *= require ../edit_gist_interface/mount_canvas
 *= require ../edit_gist_interface/mount_editor
 *= require_tree ../edit_gist_interface
-*= require tooltips
+*= require ../../tooltips
 */
